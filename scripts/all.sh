abea533ebb8c4d67ba5be6ae03dd52a680cb2cb8
aabed <ahmad.aabed.m@gmail.com>
2016-06-21 20:02:36 +0200

    Control script and enhancments for EC2

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,4 +1,1 @@
-./provision_ec2.sh  
-./provision_elk.sh
-./provision_blog.sh  
-
+./provision_ec2.sh  && ./provision_elk.sh && ./provision_blog.sh
---
+++ scripts/control.sh
@@ -0,0 +1,23 @@
+
+case $1 in
+    start_elk_machine)
+    ansible-playbook -i localhost ../ansible/provision_ec2.yml --private-key=~/Downloads/cake.pem --skip-tags "blog_box"
+    shift
+    ;;
+    start_blog_machine)
+    ansible-playbook -i localhost ../ansible/provision_ec2.yml --private-key=~/Downloads/cake.pem --skip-tags "elk_box"
+    shift
+    ;;
+    provision_elk)
+    ansible-playbook -i ../ansible/ec2.py ../ansible/provision_elk.yml --private-key=~/Downloads/cake.pem
+    shift
+    ;;
+    provision_blog)
+    ansible-playbook -i ../ansible/ec2.py ../ansible/provision_blog.yml --private-key=~/Downloads/cake.pem
+    shift
+    ;;
+    *)
+    echo "please provide one of the following [start_elk_machine,start_blog_machine,provision_elk,provision_blog]"
+    ;;
+esac
+shift # past argument or value
